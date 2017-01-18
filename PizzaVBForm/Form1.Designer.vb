e8c9357e50844176d872b7a1551d224029e9b399
Johnny Hopkins <jhopkins1009@student.isothermal.edu>
2017-01-18 15:29:28 -0500

    Add files via upload

---
+++ PizzaVBForm/Form1.Designer.vb
@@ -0,0 +1,205 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtFirst = New System.Windows.Forms.TextBox()
+        Me.txtLast = New System.Windows.Forms.TextBox()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.txtPhone = New System.Windows.Forms.TextBox()
+        Me.Takeout = New System.Windows.Forms.RadioButton()
+        Me.Delivery = New System.Windows.Forms.RadioButton()
+        Me.DineIn = New System.Windows.Forms.RadioButton()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(180, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(359, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Welcome to my Pizza Order Taking program, please enter your order below"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(68, 70)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "First Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(68, 115)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Last Name"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(68, 164)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(45, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Address"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(68, 212)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(38, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Phone"
+        '
+        'txtFirst
+        '
+        Me.txtFirst.Location = New System.Drawing.Point(153, 70)
+        Me.txtFirst.Name = "txtFirst"
+        Me.txtFirst.Size = New System.Drawing.Size(180, 20)
+        Me.txtFirst.TabIndex = 5
+        '
+        'txtLast
+        '
+        Me.txtLast.Location = New System.Drawing.Point(153, 115)
+        Me.txtLast.Name = "txtLast"
+        Me.txtLast.Size = New System.Drawing.Size(180, 20)
+        Me.txtLast.TabIndex = 6
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Location = New System.Drawing.Point(153, 161)
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(180, 20)
+        Me.txtAddress.TabIndex = 7
+        '
+        'txtPhone
+        '
+        Me.txtPhone.Location = New System.Drawing.Point(153, 205)
+        Me.txtPhone.Name = "txtPhone"
+        Me.txtPhone.Size = New System.Drawing.Size(180, 20)
+        Me.txtPhone.TabIndex = 8
+        '
+        'Takeout
+        '
+        Me.Takeout.AutoSize = True
+        Me.Takeout.Location = New System.Drawing.Point(449, 117)
+        Me.Takeout.Name = "Takeout"
+        Me.Takeout.Size = New System.Drawing.Size(65, 17)
+        Me.Takeout.TabIndex = 9
+        Me.Takeout.TabStop = True
+        Me.Takeout.Text = "Takeout"
+        Me.Takeout.UseVisualStyleBackColor = True
+        '
+        'Delivery
+        '
+        Me.Delivery.AutoSize = True
+        Me.Delivery.Location = New System.Drawing.Point(449, 151)
+        Me.Delivery.Name = "Delivery"
+        Me.Delivery.Size = New System.Drawing.Size(63, 17)
+        Me.Delivery.TabIndex = 10
+        Me.Delivery.TabStop = True
+        Me.Delivery.Text = "Delivery"
+        Me.Delivery.UseVisualStyleBackColor = True
+        '
+        'DineIn
+        '
+        Me.DineIn.AutoSize = True
+        Me.DineIn.Location = New System.Drawing.Point(449, 186)
+        Me.DineIn.Name = "DineIn"
+        Me.DineIn.Size = New System.Drawing.Size(59, 17)
+        Me.DineIn.TabIndex = 11
+        Me.DineIn.TabStop = True
+        Me.DineIn.Text = "Dine In"
+        Me.DineIn.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(226, 272)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 12
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(350, 271)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 13
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(703, 326)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.DineIn)
+        Me.Controls.Add(Me.Delivery)
+        Me.Controls.Add(Me.Takeout)
+        Me.Controls.Add(Me.txtPhone)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.txtLast)
+        Me.Controls.Add(Me.txtFirst)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtFirst As TextBox
+    Friend WithEvents txtLast As TextBox
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents txtPhone As TextBox
+    Friend WithEvents Takeout As RadioButton
+    Friend WithEvents Delivery As RadioButton
+    Friend WithEvents DineIn As RadioButton
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+End Class
